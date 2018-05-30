4f4e8f47d9701b0907eebd31ecf840f9927c0b99
klrqn <burtonsb@gmail.com>
2018-05-30 22:26:15 +0000

    figured out browser issue

---
+++ main.js
@@ -0,0 +1,15 @@
+var api = "https://fcc-weather-api.glitch.me/api/current?";
+var lat;
+var lon;
+
+
+
+$(document).ready(function() {
+
+	$.getJSON("https://ipinfo.io", function(data){
+
+		var location = data.location;
+	})
+
+
+});
