d3c57acae60c6741f2f237ce48d0ba03c6bfb97d
klrqn <burtonsb@gmail.com>
2018-06-03 16:58:58 +0000

    er

--- app.js
+++ app.js
@@ -1,91 +1,74 @@
-var lat;
-var long;
+var temp;
+var location;
+var icon;
+var humidity;
+var wind;
 
-$(document).ready(function() {
+var latitude;
+var longitude;
+var apiCall;
 
-	function getLocation(){
+function update(weather) {
+	temp.innerHTML = weather.temp;
+	location.innerHTML = weather.location;
+	humidity.innerHTML = weather.humidity;
+	wind.innerHTML = weather.wind;
+	icon.src = `imgs/code/${weather.icon}.png`;
 
-	var options = {
-	  enableHighAccuracy: true,
-	  timeout: 5000,
-	  maximumAge: 0
-	};
-
-	function success(pos) {
-	  var crd = pos.coords;
-
-	  console.log('Your current position is:');
-	  console.log(`Latitude : ${crd.latitude}`);
-	  console.log(`Longitude: ${crd.longitude}`);
-	  console.log(`More or less ${crd.accuracy} meters.`);
-	  lat = crd.latitude;
-	  long = crd.longitude;
-
-	}
-
-	function error(err) {
-	  console.warn(`ERROR(${err.code}): ${err.message}`);
-	}
-
-	navigator.geolocation.getCurrentPosition(success, error, options);
-
-	}
-});
-
-// 	getLocation();
-
-// 	if (navigator.geolocation) {
-// 		navigator.geolocation.getCurrentPosition(function(position){
-
-// 			lat = position.coords.latitude;
-// 			long = position.coords.longitude;
-// 		});
-
-// 		var weatherURL = "https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+long;
-// 		console.log(weatherURL);
-
-// 		$.getJSON(weatherURL, function(data){
-
-// 			const f = (data.main.temp * 1.8) +32;
-// 			var icon = data.weather[0].icon;
-// 			$("#temp").html(parseInt(f));
-// 			$("#temp").append(parseInt(f));
-
-// 		});
-
-// 	}
-
-// });
-
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(function (positon) {
-		long = positon.coords.longitude;
-		lat = positon.coords.latitude;
-
-		var api = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long;
-		$.getJSON(api, function (data) {
-			const f = (data.main.temp * 1.8) + 32;
-			var icon = data.weather[0].icon;
-			var img = "<img src=" + icon + ">";
-
-			$("#city").html(data.name);
-			$("#img").html(img);
-			$("#cond").html(data.weather[0].main);
-			$("#temp").html(parseInt(f) + "&#176");
-			$("#toggle").attr("value", "F");
-			$("#toggle").on("click", function () {
-
-				if (document.getElementById("toggle").value == "F") {
-					$("#temp").html(parseInt(data.main.temp) + "&#176");
-					$("#toggle").attr("value", "C");
-				}
-				else {
-					if (document.getElementById("toggle").value == "C") {
-						$("#temp").html(parseInt(f) + "&#176");
-						$("#toggle").attr("value", "F");
-					}
-				}
-			});
-		});
-	});
 }
+
+window.onload = function() {
+	/* code here */
+	// temp = $("#temperature");
+	// location = $("#location");
+	// icon = $("#icon");
+	// humidity = $("#humidity");
+	// wind = $("#wind");
+
+	// var weather = {};
+	// weather.temp = 70;
+	// weather.location = "Providence";
+	// weather.humidity = `5%`;
+	// weather.wind = 3.5;
+	// weather.icon = 200;
+	function yourWeather(latitude, longitude) {
+		var api = apiCall;
+
+		$.ajax({
+
+			url: api,
+			success: function(forecast){
+				$("#location").text(forecast.name);
+
+				var tempC = Math.round(forecast.main.temp);
+				var tempF = Math.round(tempC * (9/5) + 32);
+
+				$("#temp").text(tempF + ' F')
+
+
+			}
+		})
+	}
+
+
+
+
+	function yourLocation() {
+
+		if (navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(function(pos){
+
+			latitude = pos.coords.latitude;
+			longitude = pos.coords.longitude;
+
+			apiCall = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
+			});
+		} else {
+			$("#title").text('No Location');
+		}
+	}
+
+
+	// update(weather);
+
+};
